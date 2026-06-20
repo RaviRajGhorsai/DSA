31b4a13cbbc9b8065a7487a5c11522e51d93968f
RaviRajGhorsai <graviraj344@gmail.com>
2026-06-20 12:33:48 +0545

    binary tree implementation for insert.

---
+++ binary_search_tree.py
@@ -0,0 +1,41 @@
+"""
+Complete the insert method of the BSTNode class. It takes a User object as input and adds it to a new node if the value doesn't already exist in the tree.
+
+If the node doesn't have a value yet, store the given value and return
+If the node's value is equal to the given value, just return, no duplicates allowed
+If the given value is less than the node's value and the node doesn't have a left child, create a new left child node with the given value and return
+If the given value is less than the node's value and the node does have a left child, recursively call insert off of that left child with the given value and return
+Since we already checked if the given value is equal to or less than the node, the value must be greater than the node. Handle whether or not the node already has a right child
+"""
+
+from typing import Any
+
+
+class BSTNode:
+    def __init__(self, val: Any = None) -> None:
+        self.left: "BSTNode | None" = None
+        self.right: "BSTNode | None" = None
+        self.val = val
+
+    def insert(self, val: Any) -> None:
+        if self.val is None:
+            self.val = val
+            return
+
+        elif self.val == val:
+            return
+
+        elif val < self.val:
+            if self.left is None:
+                self.left = BSTNode(val)
+                return
+            else:
+                self.left.insert(val)
+                return
+        else:
+            if self.right is None:
+                self.right = BSTNode(val)
+                return
+            
+            self.right.insert(val)
+            return
