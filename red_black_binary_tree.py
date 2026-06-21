45ddac2792cd2382b8b7ed5348b52ab04c055ddc
RaviRajGhorsai <graviraj344@gmail.com>
2026-06-21 15:57:26 +0545

    implementation of red black algotithm to balance the binary tree, implement rotate right and left.

--- red_black_binary_tree.py
+++ red_black_binary_tree.py
@@ -18,6 +18,60 @@ class RBTree:
         self.nil.right = None
         self.root = self.nil
 
+    def rotate_left(self, pivot_parent: RBNode) -> None:
+        if pivot_parent == self.nil or pivot_parent.right == self.nil:
+            return
+
+        pivot = pivot_parent.right
+
+        pivot_parent.right = pivot.left
+
+        if pivot.left != self.nil:
+            pivot.left.parent = pivot_parent
+
+        pivot.parent = pivot_parent.parent
+
+        if pivot_parent == self.root:
+            self.root = pivot
+            
+        elif pivot_parent == pivot_parent.parent.left:
+            pivot_parent.parent.left = pivot
+
+        elif pivot_parent == pivot_parent.parent.right:
+            pivot_parent.parent.right = pivot
+
+        pivot.left = pivot_parent
+        pivot_parent.parent = pivot
+
+
+        
+
+
+    def rotate_right(self, pivot_parent: RBNode) -> None:
+        if pivot_parent == self.nil or pivot_parent.left == self.nil:
+            return
+
+        pivot = pivot_parent.left
+
+        pivot_parent.left = pivot.right
+
+        if pivot.right != self.nil:
+            pivot.right.parent = pivot_parent
+
+        pivot.parent = pivot_parent.parent
+
+        if pivot_parent == self.root:
+            self.root = pivot
+            
+        elif pivot_parent == pivot_parent.parent.right:
+            pivot_parent.parent.right = pivot
+
+        elif pivot_parent == pivot_parent.parent.left:
+            pivot_parent.parent.left = pivot
+
+        pivot.right = pivot_parent
+        pivot_parent.parent = pivot
+
     def insert(self, val: Any) -> None:
         new_node = RBNode(val)
         new_node.left = self.nil
