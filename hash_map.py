00e8d576e6f794c80b778d3988235801abf6a538
RaviRajGhorsai <graviraj344@gmail.com>
2026-06-21 18:08:03 +0545

    implementing mini hashmap, implementing hashmap resize so as our data increase space in hashmap also increase.

--- hash_map.py
+++ hash_map.py
@@ -2,6 +2,41 @@ from typing import Any
 
 
 class HashMap:
+
+    def resize(self) -> None:
+        length = len(self.hashmap)
+
+        if length == 0:
+            self.hashmap = [None]
+            return
+        current_load = self.current_load()
+
+        if current_load < 0.05:
+            return
+        else:
+            temp = self.hashmap
+            
+            self.hashmap =  [None for _ in range(length * 10)] 
+            
+            for item in temp:
+                if item is not None:
+                    key, value = item
+                    self.insert(key, value)
+
+
+
+    def current_load(self) -> float:
+        if len(self.hashmap) == 0:
+            return 1
+        
+        else:
+            filled_buckets = 0
+            for key in self.hashmap:
+                if key is not None:
+                    filled_buckets += 1
+
+            return filled_buckets/len(self.hashmap)
+
     def get(self, key: str) -> Any:
         index = self.key_to_index(key)
         
@@ -11,6 +46,8 @@ class HashMap:
             raise Exception("sorry, key not found")
 
     def insert(self, key: str, value: Any) -> None:
+        self.resize()
+
         index = self.key_to_index(key)
 
         self.hashmap[index] = (key, value)
