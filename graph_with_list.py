ea4cbe9806fa2ff34bf92e1ec89bbb290cc33d89
RaviRajGhorsai <graviraj344@gmail.com>
2026-06-24 13:05:19 +0545

    implementing graph using dictioanry, implementing BFS search..

--- graph_with_list.py
+++ graph_with_list.py
@@ -2,9 +2,30 @@
 Here we use adjacency list(using dictionary) instead of adjacency matrix(using list) like in graph.py
 
 """
+
+
 class Graph:
     def __init__(self) -> None:
-        self.graph = {} 
+        self.graph = {}
+
+    def breadth_first_search(self, v: str) -> list[str]:
+        visited_vertices = []
+        queue = []
+
+        queue.append(v)
+
+        while len(queue) > 0:
+            nexts = queue[0]
+
+            del queue[0]
+            visited_vertices.append(nexts)
+            neighbours = sorted(self.graph[nexts])
+
+            for n in neighbours:
+                if n not in visited_vertices and n not in queue:
+                    queue.append(n)
+
+        return visited_vertices
 
     def unconnected_vertices(self) -> list[int]:
         unconnected = []
@@ -15,7 +36,7 @@ class Graph:
         return unconnected
 
     def adjacent_nodes(self, node: int) -> set[int]:
-        return self.graph.get(node, set()) 
+        return self.graph.get(node, set())
 
     def add_edge(self, u: int, v: int) -> None:
         if u not in self.graph:
@@ -31,11 +52,9 @@ class Graph:
         if u not in self.graph:
             self.graph[u] = set()
 
-
     # don't touch below this line
 
     def edge_exists(self, u: int, v: int) -> bool:
         if u in self.graph and v in self.graph:
             return (v in self.graph[u]) and (u in self.graph[v])
         return False
-
